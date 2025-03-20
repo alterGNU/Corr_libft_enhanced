01dca2f9fafe090f2e416948e4d8f4c3b83e0a32
alterGNU <alterGNU@gmail.com>
2025-03-20 01:16:58 +0100

    ADD test ft_strcspn()

---
+++ src/tests_libft/string/test_ft_strcspn.c
@@ -0,0 +1,59 @@
+#include "libft.h"  // ft_strcspn
+#include <string.h> // strcspn
+#include <stdio.h>  // printf
+#define LEN 100
+
+int	printntime(char c, int n)
+{
+	for (int i = 0 ; i < n; i++)
+		printf("%c", c);
+	return (n);
+}
+
+int	test(char *src, char *accept)
+{
+	int	print_sofar;
+	int	ft;
+	int	real;
+	ft = ft_strcspn(src, accept);
+	if (!src || !accept)
+	{
+		print_sofar=printf("strcspn(\"%s\", \"%s\")ft-> [%d]", src, accept, ft);
+		if (ft == -1)
+			return (printntime('-', LEN-print_sofar-7), printf("> ✅\n"), 0);
+		return (printntime('-', LEN-print_sofar-7), printf("> ❌\n"), 0);
+	}
+	real = (int)strcspn(src, accept);
+	print_sofar=printf("strcspn(\"%s\", \"%s\")[%d] vs [%d]", src, accept, real, ft);
+	if (real == ft)
+		return (printntime('-', LEN-print_sofar-7), printf("> ✅\n"), 0);
+	return (printntime('-', LEN-print_sofar-7), printf("> ❌\n"), 0);
+}
+
+int main()
+{
+	int	nb_err = 0;
+	nb_err += test(NULL, NULL);
+	nb_err += test("coucou ", NULL);
+	nb_err += test(NULL, " ");
+
+	nb_err += test("", "");
+	nb_err += test("abcdefghijklmopqrstuvwxyz", "");
+	nb_err += test("coucou", "");
+	nb_err += test("    c", " ");
+	nb_err += test("    c", "c");
+	nb_err += test("coucou", "c");
+	nb_err += test("av1sd2cw34", "0123456789");
+	nb_err += test("clkx1lxka34", "0123456789");
+	nb_err += test("coucou ", "x ");
+
+	char	src1[10]="123123123";
+	char	src2[10]="12312312 ";
+	for (int i = 0; i < 10; i++)
+	{
+		nb_err += test(src1+i, "asd345");
+		nb_err += test(src2+i, " ");
+	}
+	return (nb_err);
+}
+
