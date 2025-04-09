11576590ad88be9b4e7e23f2e987eccdc25670e9
alterGNU <alterGNU@gmail.com>
2025-04-09 15:24:46 +0200

    WIP:DEBBUG:segfault while testing test_ft_btreedelone

--- src/my_unitests/btree/test_ft_btreedelone.c
+++ src/my_unitests/btree/test_ft_btreedelone.c
@@ -1,6 +1,14 @@
 #include "libft.h"  // struct t_btree
-#include "string.h" // strcmp, strdup
+#include <string.h> // strcmp, strdup
+#include <stdio.h> // strcmp, strdup
 #define LEN 90
+#define CE "\033[0m"    // COLOR END
+#define CR "\033[0;31m" // COLOR RED
+#define CV "\033[0;32m" // COLOR GREEN
+#define CM "\033[0;33m" // COLOR BROWN
+#define CY "\033[0;93m" // COLOR YELLOW
+#define CB "\033[0;36m" // COLOR AZURE
+#define CG "\033[0;37m" // COLOR GREY
 #define PASS "> \033[37;42m ✓ \033[0m\n"
 #define	FAIL "> \033[30;41m ✗ \033[0m\n"
 
@@ -85,25 +93,6 @@ int	print_btree_content_is_chararray(t_btree *btree_node)
 	return (print_sofar);
 }
 
-void free_btree_node_chararray(void *ptr)
-{
-	t_btree *node = ptr;
-	if (!node)
-		return;
-	if (node->content)
-	{
-		while (node->content)
-		{
-			ft_printf("str=%s\n", node->content);
-			ft_free(node->content);
-			node->content = NULL;
-			node->content++;
-		}
-	}
-	free(node);
-	node = NULL;
-}
-
 //int	ft_btreedelone(t_btree **btree_add, void (*del)(void *))
 int	test_chararray(const char **array)
 {
@@ -144,50 +133,70 @@ int	test_chararray(const char **array)
 			exit(42);
 		}
 	}
+	// Create node, and check if constructor worked
 	node = ft_btreenew(content);
+	if (!node)
+	{
+		ft_free_str_array(&content);
+		ft_fprintf(STDERR_FILENO, "ft_btreenew() returns NULL");
+		exit(42);
+	}
+	if (content && !node->content)
+	{
+		ft_free_str_array(&content);
+		ft_fprintf(STDERR_FILENO, "ft_btreenew(content) fails to build ->content");
+		exit(42);
+	}
+	// Print newly created node
 	ft_printf("\nbtree_node = ");
 	print_btree_content_is_chararray(node);
 	ft_printf("\n");
-	// Check results
-	if (!node || !node->content)
-		return (ft_btreedelone(&node, free_btree_node_chararray), printntime('-', LEN - 5), ft_printf(FAIL), 1);
-	i = 0;
+	// Check node->content == arr
 	char **act = (char **)(node->content);
+	int len_node = 0;
+	while (act[len_node])
+		len_node++;
+	int len_arr = 0;
+	while (array[len_arr])
+		len_arr++;
+	if (len_arr != len_node)
+	{
+		ft_btreedelone(&node, ft_free_str_array);
+		printntime('-', LEN - 5);
+		ft_printf(FAIL);
+		return (1);
+	}
+	i = 0;
 	while (act[i] && array[i])
 	{
 		if (strcmp(act[i], array[i]))
-			return (ft_btreedelone(&node, free_btree_node_chararray), printntime('-', LEN - 5), ft_printf(FAIL), 1);
+			return (ft_btreedelone(&node, ft_free_str_array), printntime('-', LEN - 5), ft_printf(FAIL), 1);
 		i++;
 	}
-	ft_printf("LA\n");
-	if (act[i] || array[i])
-	{
-		ft_printf("LA1\n");
-		return (ft_btreedelone(&node, free_btree_node_chararray), printntime('-', LEN - 5), ft_printf(FAIL), 1);
-	}
-	ft_printf("LA2\n");
-	return (ft_btreedelone(&node, free_btree_node_chararray), printntime('-', LEN - 5), ft_printf(PASS), 0);
+	if (act[len_node] || array[len_arr])
+		return (ft_btreedelone(&node, ft_free_str_array), printntime('-', LEN - 5), ft_printf(FAIL), 1);
+	return (ft_btreedelone(&node, ft_free_str_array), printntime('-', LEN - 5), ft_printf(PASS), 0);
 }
 
 int	main()
 {
 	int	nb_err=0;
-	// =[ CONTENT == STRING ]===================================================
-	print_title("Content == string");
-	nb_err+=test_char(NULL);          // content == NULL
-	nb_err+=test_char("");            // content == empty string
-	nb_err+=test_char("0a");          // content != empty string
-	nb_err+=test_char(" 0a");         // content != empty string
-	nb_err+=test_char("01234 abcde"); // content != empty string
+	//// =[ CONTENT == STRING ]===================================================
+	//print_title("Content == string");
+	//nb_err+=test_char(NULL);          // content == NULL
+	//nb_err+=test_char("");            // content == empty string
+	//nb_err+=test_char("0a");          // content != empty string
+	//nb_err+=test_char(" 0a");         // content != empty string
+	//nb_err+=test_char("01234 abcde"); // content != empty string
 	// =[ CONTENT == STRING ARRAY ]=============================================
 	print_title("Content == string arrays");
-	nb_err+=test_chararray(NULL);    // arr  == NULL
-	const char *t0[1] = {NULL};
-	nb_err+=test_chararray(t0);      // *arr == NULL
-	const char *t1[2] = {"", NULL};
-	nb_err+=test_chararray(t1);      // arr not empty but str empty
-	const char *t2[4] = {"", "", "", NULL};
-	nb_err+=test_chararray(t2);      // arr not empty but full of str empty
+	//nb_err+=test_chararray(NULL);    // arr  == NULL
+	//const char *t0[1] = {NULL};
+	//nb_err+=test_chararray(t0);      // *arr == NULL
+	//const char *t1[2] = {"", NULL};
+	//nb_err+=test_chararray(t1);      // arr not empty but str empty
+	//const char *t2[4] = {"", "", "", NULL};
+	//nb_err+=test_chararray(t2);      // arr not empty but full of str empty
 	const char *t3[4] = {"coucou", "petite", "perruche", NULL};
 	nb_err+=test_chararray(t3);      // arr and its str not empty
 	return (nb_err);
